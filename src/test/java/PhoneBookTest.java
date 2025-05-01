468c35c53d2fea6b3affec2dd02b01ccbbcd41f0
Artos test <korshenkov.vlad@yandex.ru>
2025-05-01 15:49:30 +0300

    Stub and tests for add

--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,2 +1,13 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class PhoneBookTest {
+
+    @Test
+    public void testAdd() {
+        PhoneBook pb = new PhoneBook();
+        int result = pb.add();
+        assertEquals(1, result);
+    }
 }
