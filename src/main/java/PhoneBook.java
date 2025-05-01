b475cb0b125b177acce82f6f1857760800cecff0
Artos test <korshenkov.vlad@yandex.ru>
2025-05-01 16:10:43 +0300

    findByNumber implemented

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -2,4 +2,8 @@ public class PhoneBook {
     public int add() {
         return 0;
     }
+
+    public String findByNumber(String number) {
+        return null;
+    }
 }
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,13 +1,25 @@
 import org.junit.Test;
-
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 public class PhoneBookTest {
 
     @Test
     public void testAdd() {
-        PhoneBook pb = new PhoneBook();
-        int result = pb.add();
+        PhoneBook phoneBook = new PhoneBook();
+        int result = phoneBook.add();
         assertEquals(0, result);
+        System.out.println("Тест 1 готов");
+
+    }
+
+    @Test
+    public void testFindByNumber() {
+        PhoneBook phoneBook = new PhoneBook();
+        String result = phoneBook.findByNumber("1234567890");
+        assertNull(result);
+        System.out.println("Тест 2 готов");
+
     }
 }
+
